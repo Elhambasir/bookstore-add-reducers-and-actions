b2a18c6a18468093eff18d08aefe75a6a6bf4f53
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-18 01:02:17 +0430

    Update booksSlice.js

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,7 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  bookItems: [],
+  bookItems: [
+    {
+      id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
 };
 
 const bookSlice = createSlice({
@@ -12,7 +32,7 @@ const bookSlice = createSlice({
       state.bookItems.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.bookItems = state.bookItems.filter((item) => item.id === action.payload);
+      state.bookItems = state.bookItems.filter((item) => item.id !== action.payload);
     },
   },
 });
