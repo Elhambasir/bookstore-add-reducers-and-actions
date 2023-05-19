6289e23fa3683e4fae50e1d980e977a3281ff6a0
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-19 17:45:04 +0430

    Update RenderIndividualBook.jsx
    1. Refactor the add book and remove book features to persist changes in the server (external API)

--- src/components/RenderIndividualBook.jsx
+++ src/components/RenderIndividualBook.jsx
@@ -1,46 +1,55 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeBookFromApi, fetchApi } from '../redux/books/booksSlice';
 import styles from '../styles/renderIndividual.module.css';
 
-export default function RenderIndividualBook({ renderData }) {
+export default function RenderIndividualBook() {
   const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
+  useEffect(() => {
+    dispatch(fetchApi());
+  }, [dispatch]);
+
+  const booksList = books.bookItems.map((book, index) => ({
+    ...book,
+    item_id: book.item_id,
+    key: `${book.item_id}-${index}`,
+  }));
+
   return (
-    <>
-      <div key={renderData.id} className={styles.renderWrapper}>
-        <div className={styles.renderWrapperBookInfo}>
-          <small className={styles.bookCategory}>
-            {renderData.category}
-          </small>
-          <h2 className={styles.bookTitle}>{renderData.title}</h2>
-          <small className={styles.bookAuthor}>
-            {renderData.author}
-          </small>
-          <div className={styles.buttons}>
-            <button type="button" id="remove">Content</button>
-            <button type="button" id={renderData.id} onClick={(e) => dispatch(removeBook(e.target.id))}>Remove</button>
-            <button type="button" id="remove">Edit</button>
+    <div>
+      {booksList.map((book) => (
+        <div key={book.key} className={styles.renderWrapper}>
+          <div className={styles.renderWrapperBookInfo}>
+            <small className={styles.bookCategory}>
+              {book.category}
+            </small>
+            <h2 className={styles.bookTitle}>{book.title}</h2>
+            <small className={styles.bookAuthor}>
+              {book.author}
+            </small>
+            <div className={styles.buttons}>
+              <button type="button" id="remove">Content</button>
+              <button type="button" id={book.item_id} onClick={() => dispatch(removeBookFromApi(book.item_id))}>Remove</button>
+              <button type="button" id="remove">Edit</button>
+            </div>
+          </div>
+          <div className={styles.renderWrapperCurrentState}>
+            <div className={styles.renderWrapperCurrentStateShape}>
+              <h4>State</h4>
+            </div>
+            <div className={styles.renderWrapperPercentage}>
+              <h1>64%</h1>
+              <small>Completed</small>
+            </div>
+          </div>
+          <div className={styles.renderWrapperCurrentChapter}>
+            <h5 className={styles.currentChapterTitle}>CURRENT CHAPTER</h5>
+            <h5 className={styles.chapterTitle}>Chapter title</h5>
+            <button type="button" className={styles.btnUpdateProgress}>UPDATE PROGRESS</button>
           </div>
         </div>
-        <div className={styles.renderWrapperCurrentState}>
-          <div className={styles.renderWrapperCurrentStateShape}>
-            <h4>State</h4>
-          </div>
-          <div className={styles.renderWrapperPercentage}>
-            <h1>64%</h1>
-            <small>Completed</small>
-          </div>
-        </div>
-        <div className={styles.renderWrapperCurrentChapter}>
-          <h5 className={styles.currentChapterTitle}>CURRENT CHAPTER</h5>
-          <h5 className={styles.chapterTitle}>Chapter title</h5>
-          <button type="button" className={styles.btnUpdateProgress}>UPDATE PROGRESS</button>
-        </div>
-      </div>
-    </>
+      ))}
+    </div>
   );
 }
-RenderIndividualBook.propTypes = {
-  renderData: PropTypes.string.isRequired,
-};
